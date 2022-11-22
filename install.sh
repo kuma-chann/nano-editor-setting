a9acda558761ea4ed2c53f44d981028cea186129
kuma-chann <118359263+kuma-chann@users.noreply.github.com>
2022-11-23 01:08:55 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+## syntax highlight
+mkdir ~/.nano
+cd ~/.nano
+# wget https://.../nano-editor-setting/c.nanorc
+# wget https://.../nano-editor-setting/python.nanorc
+
+## .nanorc
+cd ~/
+# wget https://.../nano-editor-setting/.nanorc
