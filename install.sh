fb1f66c08b5d509643f8acead32ba4dbbf983b34
kuma-chann <118359263+kuma-chann@users.noreply.github.com>
2022-11-23 04:02:08 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 mkdir ~/.nano
 cd ~/.nano
 wget https://raw.githubusercontent.com/kuma-chann/nano-editor-setting/main/c.nanorc
-# wget https://raw.githubusercontent.com/kuma-chann/nano-editor-setting/main/python.nanorc
 
 ## .nanorc
 cd ~/
